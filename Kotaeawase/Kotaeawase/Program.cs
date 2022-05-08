c5e9898fe05a2cf1d759ad10be9fb1c075331be6
ShoTakesako <shotakesako@outlook.jp>
2022-05-08 13:34:34 -0400

    Model

--- Kotaeawase/Kotaeawase/Program.cs
+++ Kotaeawase/Kotaeawase/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+
+namespace kotaeawase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string? subject;
+            WriteLine("答え合わせシステムへようこそ!");
+            bool complete = false;
+            while (complete == false)
+            {
+                WriteLine("教科を選んでください");
+                WriteLine("数学・国語");
+                subject = ReadLine();
+                if (subject == "数学")
+                {
+                    complete = true;
+                }
+                else if (subject == "国語")
+                {
+                    complete = true;
+                }
+                else
+                {
+                }
+            }
+            WriteLine("ご利用ありがとうございました。");
+            ReadKey();
+        }
+    }
+}
